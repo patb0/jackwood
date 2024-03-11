eb02b74ce72b09c4518ab21e8326ff4a636331ea
PatrykB <p.boguslawski@netland.pl>
2024-03-11 21:28:59 +0100

    Resize realisations images

---
+++ src/app/models/realisations.ts
@@ -0,0 +1,50 @@
+import { Project } from "./project";
+
+export class Realisations {
+    projects: Project[] = [
+        {
+            title: "ALTANY",
+            imagesUrls: [
+                "./assets/images/realisations/gazebos/base.jpeg",
+                "./assets/images/realisations/gazebos/1.jpg",
+                "./assets/images/realisations/gazebos/2.jpg",
+                "./assets/images/realisations/gazebos/3.jpeg",
+                "./assets/images/realisations/gazebos/4.jpeg",
+            ],
+        },
+        {
+            title: "DOMY SZKIELETOWE/DOMKI",
+            imagesUrls: [
+                "./assets/images/realisations/houses/base.jpeg",
+                "./assets/images/realisations/houses/1.jpeg",
+                "./assets/images/realisations/houses/2.jpeg",
+                "./assets/images/realisations/houses/4.jpeg",
+                "./assets/images/realisations/houses/5.jpeg",
+                "./assets/images/realisations/houses/6.jpeg",
+                "./assets/images/realisations/houses/7.jpeg",
+                "./assets/images/realisations/houses/8.jpeg",
+                "./assets/images/realisations/houses/9.jpeg",
+                "./assets/images/realisations/houses/10.jpeg",
+                "./assets/images/realisations/houses/11.jpeg",
+            ],
+        },
+        {
+            title: "DACHY",
+            imagesUrls: [
+                "./assets/images/realisations/roofs/base.jpeg",
+                "./assets/images/realisations/roofs/2.jpeg",
+                "./assets/images/realisations/roofs/3.jpeg",
+                "./assets/images/realisations/roofs/4.jpeg",
+            ],
+        },
+        {
+            title: "WIATY",
+            imagesUrls: [
+                "./assets/images/realisations/sheds/base.jpeg",
+                "./assets/images/realisations/sheds/1.jpeg",
+                "./assets/images/realisations/sheds/3.jpeg",
+                "./assets/images/realisations/sheds/4.jpeg",
+            ],
+        }
+    ];
+}
